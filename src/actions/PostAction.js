78248b8b41953420372fc4583437ff99f93f9ed6
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2019-05-21 11:27:31 +0530

    Added search action

--- src/actions/PostAction.js
+++ src/actions/PostAction.js
@@ -1,12 +1,17 @@
 import ApiConstants from '../constants/ApiConstants'
 
 import { POSTS_FETCHED } from '../constants/Constants'
+import { POSTS_SEARCHED } from '../constants/Constants'
 
 export function setPosts(posts=[]) {
     return {
         type: POSTS_FETCHED,
         posts
     }
+}
+
+export function search(value) {
+    return {type: POSTS_SEARCHED, value};
 }
 
 export function fetchPosts() {
@@ -17,3 +22,12 @@ export function fetchPosts() {
         .then(data => dispatch(setPosts(data)));
     }
 }
+
+/*export function searchPosts() {
+    const { baseURL, POSTS_URL } = ApiConstants;
+    return dispatch => {
+        fetch(`${baseURL}${POSTS_URL}`)
+        .then(res => res.json())
+        .then(data => dispatch(setPosts(data)));
+    }
+}*/
--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,2 +1,3 @@
 export const LOGIN = 'LOGIN'
 export const POSTS_FETCHED = 'POSTS_FETCHED'
+export const POSTS_SEARCHED = 'POSTS_SEARCHED'
